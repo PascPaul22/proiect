73537d64bbf1326eaab7d483376756d8d5fbccaa
Pașc Paul <paulpasc22@gmail.com>
2022-12-12 19:46:09 +0200

    Update proiect.cpp

--- proiect/proiect.cpp
+++ proiect/proiect.cpp
@@ -2,3 +2,12 @@
 #include<cmath>
 using namespace std;
 int nr;
+void H(int n, char a, char b, char c)
+{
+	if (n >= 1)
+	{
+		H(n - 1, a, c, b);
+		cout << a << "->" << c << endl;
+		H(n - 1, b, a, c);
+	}
+}
